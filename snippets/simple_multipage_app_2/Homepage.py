8ea79094a5e665541ae80cff8d05ce55d0a6b154
Sammi Rosser <samanthajaderosser@gmail.com>
2024-08-20 16:03:48 +0100

    Add example multipage app

---
+++ snippets/simple_multipage_app_2/Homepage.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Homepage")
+
+st.title("Welcome to the penguin app!")
+
+species_options = ["Gentoo", "Chinstrap", "Adelie"]
+
+chosen_species = st.selectbox("Which penguin species are you interested in finding out more about?", species_options)
+
+if chosen_species == "Gentoo":
+    st.write(
+        """
+        The gentoo penguin (JEN-too) (Pygoscelis papua) is a penguin species (or possibly a species complex) in the genus Pygoscelis, most closely related to the Adélie penguin (P. adeliae) and the chinstrap penguin (P. antarcticus). The earliest scientific description was made in 1781 by Johann Reinhold Forster with a type locality in the Falkland Islands. The species calls in a variety of ways, but the most frequently heard is a loud trumpeting, which the bird emits with its head thrown back.
+        """
+    )
+elif chosen_species == "Chinstrap":
+    st.write(
+        """
+The chinstrap penguin (Pygoscelis antarcticus) is a species of penguin that inhabits a variety of islands and shores in the Southern Pacific and the Antarctic Oceans. Its name stems from the narrow black band under its head, which makes it appear as if it were wearing a black helmet, making it easy to identify.[2] Other common names include ringed penguin, bearded penguin, and stonecracker penguin, due to its loud, harsh call.[3]
+        """
+    )
+
+elif chosen_species == "Adelie":
+    st.write(
+        """
+        The Adélie penguin (Pygoscelis adeliae) is a species of penguin common along the entire coast of the Antarctic continent, which is the only place where it is found. It is the most widespread penguin species, and, along with the emperor penguin, is the most southerly distributed of all penguins. It is named after Adélie Land, in turn, named for Adèle Dumont d'Urville, who was married to French explorer Jules Dumont d'Urville, who first discovered this penguin in 1840. Adélie penguins obtain their food by both predation and foraging, with a diet of mainly krill and fish.
+        """
+    )
+
+st.markdown("*All information from wikipedia*")
---
+++ snippets/simple_multipage_app_2/pages/2_Penguin_Trends.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+import streamlit as st
+from palmerpenguins import load_penguins
+import plotly.express as px
+
+st.write("All of the things we've learned about so far can be used within a multipage app!")
+
+st.write("Just remember that each page is effectively a self-contained app - without using things like session state, we can't ")
+
+tab1, tab2 = st.tabs(["Penguin Graphs", "Video"])
+
+penguins = load_penguins()
+
+with tab1:
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        fig = px.scatter(penguins, x='bill_length_mm', y='bill_depth_mm', color="sex",
+        title=f"Penguins Dataset - Bill Length (mm) vs Bill Depth (mm), coloured by Sex")
+
+        st.plotly_chart(fig)
+
+        with st.expander("Click here to see the code for the graph"):
+            st.code(
+              """
+              fig = px.scatter(penguins, x='bill_length_mm', y='bill_depth_mm', color="sex",
+                    title=f"Penguins Dataset - Bill Length (mm) vs Bill Depth (mm), coloured by Sex")
+              """
+            )
+
+    with col2:
+        fig = px.scatter(penguins, x='flipper_length_mm', y='body_mass_g', color="species",
+                    title=f"Penguins Dataset - Flipper Length (mm) vs Body Weight(g), coloured by Species")
+
+        st.plotly_chart(fig)
+
+        with st.expander("Click here to see the code for the graph"):
+            st.code(
+              """
+              fig = px.scatter(penguins, x='flipper_length_mm', y='body_mass_g', color="species",
+                    title=f"Penguins Dataset - Flipper Length (mm) vs Body Weight(g), coloured by Species")
+              """
+            )
+
+    with st.expander("Click here to see the underlying data"):
+        st.dataframe(penguins)
+
+with tab2:
+    st.header("Penguin Video")
+
+    expander_video = st.expander("Click here to watch a penguin video")
+    expander_video.video("https://www.youtube.com/watch?v=nFAK8Vj62WM")
